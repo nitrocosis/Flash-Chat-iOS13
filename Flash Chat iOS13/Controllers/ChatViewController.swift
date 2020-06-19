ef5c1291998625b53517aa140809113b1c7cd326
Sarah Estrellado <Sarah.estrellado@icloud.com>
2020-06-19 15:05:55 -0500

    sorting messages by date

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -30,8 +30,13 @@ class ChatViewController: UIViewController {
     }
     
     func loadMessages() {
-        messages = []
-        db.collection(K.FStore.collectionName).getDocuments { (querySnapshot, error) in
+        
+        db.collection(K.FStore.collectionName)
+            .order(by: K.FStore.dateField)
+            .addSnapshotListener { (querySnapshot, error) in
+           
+            self.messages = []
+            
             if let e = error {
                 print(e.localizedDescription)
             } else {
@@ -57,7 +62,8 @@ class ChatViewController: UIViewController {
         if let messageBody = messageTextfield.text , let messageSender = Auth.auth().currentUser?.email {
             db.collection(K.FStore.collectionName).addDocument(data: [
                 K.FStore.senderField: messageSender,
-                K.FStore.bodyField: messageBody]) { (error) in
+                K.FStore.bodyField: messageBody,
+                K.FStore.dateField: Date().timeIntervalSince1970]) { (error) in
                     if let e = error {
                         print(e.localizedDescription)
                     } else {
